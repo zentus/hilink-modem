d2b31e31b31d1fe6984a35a1396d7b73fb7ae891
zentus <zentus@cimantra.com>
2020-03-26 17:10:20 +0100

    Use Modem.apiRequest() internally (DRY) and minor fixes

--- example.js
+++ example.js
@@ -8,39 +8,42 @@ const modem = new Modem({
 	// waitForPendingRequest: true
 })
 
-// Send SMS message
-modem.sendMessage({
-	receiver: '+46736000000',
-	text: 'hello world'
-}).then(status => console.log(status))
+// modem.apiRequest('/cradle/basic-info').then(console.log)
+modem.getMailbox().then(console.log)
 
-// Get SMS Inbox
-modem.getInbox({
-	page: 1,
-	count: 1,
-	sort: 'ascending'
-}).then(messages => console.log(messages))
-
-// Get SMS Outbox
-modem.getOutbox({
-	// count: 20
-	// page: 1
-	// sort: 'descending'
-}).then(messages => console.log(messages))
-
-// Listen for new SMS messages
-modem.onMessage(async message => {
-	console.log('recieved message:', message)
-
-	const pingOrPong = (
-		(message.text.toUpperCase() === 'PING' && 'PONG') ||
-		(message.text.toUpperCase() === 'PONG' && 'PING')
-	)
-
-	const status = await modem.sendMessage({
-		receiver: message.sender,
-		text: pingOrPong || `Let's play ping pong!`
-	})
-
-	console.log('sent message:', status)
-})
+// // Send SMS message
+// modem.sendMessage({
+// 	// receiver: '+46736000000',
+// 	// text: 'hello world'
+// }).then(status => console.log(status))
+//
+// // Get SMS Inbox
+// modem.getInbox({
+// 	// page: 1,
+// 	// count: 1,
+// 	// sort: 'ascending'
+// }).then(messages => console.log(messages))
+//
+// // Get SMS Outbox
+// modem.getOutbox({
+// 	// count: 20
+// 	// page: 1
+// 	// sort: 'descending'
+// }).then(messages => console.log(messages))
+//
+// // Listen for new SMS messages
+// modem.onMessage(async message => {
+// 	console.log('recieved message:', message)
+//
+// 	const pingOrPong = (
+// 		(message.text.toUpperCase() === 'PING' && 'PONG') ||
+// 		(message.text.toUpperCase() === 'PONG' && 'PING')
+// 	)
+//
+// 	const status = await modem.sendMessage({
+// 		receiver: message.sender,
+// 		text: pingOrPong || `Let's play ping pong!`
+// 	})
+//
+// 	console.log('sent message:', status)
+// })
